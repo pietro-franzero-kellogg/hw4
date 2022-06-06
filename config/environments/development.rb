4753ce1e4b153860c3aa362720a2ac365b502c53
pietro-franzero-kellogg <pietro.franzero@kellogg.northwestern.edu>
2022-06-06 05:37:36 +0000

    Adding file attachment feature

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  config.active_storage.service = :local
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
@@ -67,4 +69,5 @@ Rails.application.configure do
   
   # So we don't get the "Cannot render console" message in Gitpod
   config.web_console.whiny_requests = false
+  
 end
